0421be313607fd53f519bee9110f55099a4832c6
VadimYarysh <yarish612@gmail.com>
2021-11-13 17:00:29 +0300

    Поставил энтити

---
+++ Project-Runners/Project-Runners.Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Runners.Data.Models;
+
+namespace Project_Runners.Data
+{
+    /// <summary>
+    /// Класс контекста БД
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt) : base(opt)
+        {
+            
+        }
+
+        /// <summary> Прогоны </summary>
+        public DbSet<Run> Runs { get; set; }
+
+        /// <summary> Кейсы </summary>
+        public DbSet<Case> Cases { get; set; }
+    }
+}
--- Project-Runners/Project-Runners.Web/Startup.cs
+++ Project-Runners/Project-Runners.Web/Startup.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Project_Runners.Data;
 
 namespace Project_Runners
 {
@@ -26,6 +29,8 @@ namespace Project_Runners
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("InMem"));
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
