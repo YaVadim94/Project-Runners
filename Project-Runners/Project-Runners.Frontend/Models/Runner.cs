95c7fd090dbb22785ad02e7f09fc9dafed1d95e3
VadimYarysh <yarish612@gmail.com>
2021-12-25 17:13:21 +0300

    Сделал страницу с раннерами на тестовых данных

---
+++ Project-Runners/Project-Runners.Frontend/Models/Runner.cs
@@ -0,0 +1,19 @@
+﻿using ProjectRunners.Common.Enums;
+
+namespace Project_Runners.Frontend.Models
+{
+    /// <summary>
+    /// Раннер
+    /// </summary>
+    public class Runner
+    {
+        /// <summary> Идентификатор </summary>
+        public long? Id { get; set; }
+
+        /// <summary> Наименование </summary>
+        public string Name { get; set; }
+
+        /// <summary> Состояние </summary>
+        public RunnerState State { get; set; }
+    }
+}
---
+++ Project-Runners/Project-Runners.Frontend/Pages/Runners.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Project_Runners.Frontend.Models;
+using ProjectRunners.Common.Enums;
+
+namespace Project_Runners.Frontend.Pages
+{
+    /// <summary>
+    /// Страница раннеров
+    /// </summary>
+    public partial class Runners : ComponentBase
+    {
+        public Runners()
+        {
+            DataSource = new List<Runner>
+            {
+                new () {Id = 1, Name = "First", State = RunnerState.Running},
+                new () {Id = 2, Name = "Second", State = RunnerState.Waiting},
+                new () {Id = 3, Name = "Third", State = RunnerState.Disconnected}
+            };
+        }
+
+        /// <summary> Раннеры </summary>
+        private ICollection<Runner> DataSource { get; set; }
+    }
+}
